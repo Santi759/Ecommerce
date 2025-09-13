7aa824bc6b14c918cd1d064c46e92fa06a76ba92
Santiago Chepilla <82857800+Santi759@users.noreply.github.com>
2025-09-13 15:30:51 -0300

    Update CarritoVacio.jsx

--- src/pages/CarritoVacio.jsx
+++ src/pages/CarritoVacio.jsx
@@ -1,14 +1,1 @@
-import React from "react";
-import{FaShoppingCart} from 'react-icons/fa'
-import './CarritoVacio.css'
 
-const EmptyCart = () =>{
-    return(
-        <div className="empty-cart-container">
-            <FaShoppingCart className="cart-icon" />
-            <p className="empty-cart-text">carrito vacío</p> 
-        </div>
-    );
-};
-
-export default EmptyCart;
